6082b3c6311d9d4709e801dfa281b9db7f01979e
Valeria Giannelli <vg.giannellivaleria@gmail.com>
2024-06-26 12:48:23 +0200

    build: create vue files

---
+++ js/vue.js
@@ -0,0 +1,33 @@
+// estrapolo il metodo del framework che ci permette di creare app Vue
+const { createApp } = Vue;
+
+// creo l'istanza di un'app VUE e la collogo all'ID contenitore 
+createApp ({
+    data(){
+        return {
+            toDoList : [
+                {
+                    text: 'fare spesa',
+                    done: false,
+                }, 
+                {
+                    text: 'esercizio',
+                    done: true,
+                }, 
+                {
+                    text: 'doccia',
+                    done: true,
+                }, 
+                {
+                    text: 'pulire casa',
+                    done: false,
+                }, 
+                {
+                    text: 'vedersi con Pippo',
+                    done: false,
+                }
+            ]
+        }
+    },
+
+}).mount("#container");
